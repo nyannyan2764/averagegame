f4ee4039624c720ecd2bcc0dba87cd4ac92de4a1
nyannyan2764 <denkistuushindaidaigakusei@gmail.com>
2025-10-05 14:07:31 +0900

    Update state.js

--- state.js
+++ state.js
@@ -3,4 +3,10 @@ export const gameState = {
     currentRound: 1,
     jokerSelections: {},
     totalPlayers: 3,
+
+    /**
+     * 現在デバイスを操作しているプレイヤーの名前を保持する
+     * ラウンド開始時にnullにリセットされ、プレイヤー選択時に設定される
+     */
+    currentUser: null, 
 };
